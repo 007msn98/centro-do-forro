d24bdd008b955bb185d26d7587a60fc45f688a5c
Daniel Gomes Assunção <106596714+007msn98@users.noreply.github.com>
2023-02-26 14:08:41 -0400

    centro do forro finalizado

---
+++ src/scripts/scripts.js
@@ -0,0 +1,15 @@
+// const makeDesapear = () => {
+//   const desapear = document.getElementById("will-desapear");
+//   const link = document.getElementById("make-desapear");
+//   link.addEventListener("click", () => {
+//     desapear.classList.add("desapear");
+//   });
+//   if(desapear.classList === 'desapear'){
+//     localStorage.setItem('cookiesAccepted', true)
+//   }
+//   else if(desapear.classList !== 'desapear'){
+//     localStorage.setItem('cookiesAccepted', false)
+//   }
+  
+// };
+// makeDesapear();
